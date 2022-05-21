dc53f8c73159bd979791e0f2f0862bf4db6b7260
juanjo <juanjose.mquesada@gmail>
2022-05-21 11:18:12 +0200

    version final

---
+++ main.py
@@ -0,0 +1,16 @@
+# Comentario
+
+print('Hola mundo')
+
+# git init
+# git add
+# git commit
+# git status
+# git reset
+# git push
+# git pull
+# git feel o feed o algo asi...
+
+print('nueva version de practica')
+
+print('otra nueva version de practica')
